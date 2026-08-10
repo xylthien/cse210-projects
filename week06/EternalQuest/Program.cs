5affe4cc707409b16566ce5a1fe1061a3c8cadb0
xylthien <xylthien@gmail.com>
2026-08-10 13:00:26 -0600

    Eternal Quest Commentary

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -2,6 +2,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        // There were a few edge casses that needed to be handled, mostly involving Console.ReadLine() for integers.
+        // I wrote GetIntergerInput() to help rule out strings or numbers outside parameters. 
         Console.Clear();
         GoalManager manager = new GoalManager();
         manager.Start();
